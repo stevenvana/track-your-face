527d19601ca02c871e7cba19b3594699f34aca6f
stevenspyramid <steven.vanasbeck@gmail.com>
2019-11-05 12:33:38 +0100

    rounded emotionData and added percentage sign

--- src/TakePicture.js
+++ src/TakePicture.js
@@ -3,7 +3,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Webcam from "react-webcam";
 import PropTypes from "prop-types";
 import Button from "@material-ui/core/Button";
-import { StyledTakePicture, StyledWebcam } from "./styled";
 import { calculateEmotionData } from "./helperFunctions";
 
 const useStyles = makeStyles(theme => ({
@@ -14,14 +13,6 @@ const useStyles = makeStyles(theme => ({
     marginRight: theme.spacing(1)
   }
 }));
-
-const ProvisionalPicture = ({ data }) => (
-  <img alt="provisionalPicture" src={data} />
-);
-
-ProvisionalPicture.propTypes = {
-  data: PropTypes.string.isRequired
-};
 
 export default function TakePicture(props) {
   const { saveEmotionData } = props;
@@ -41,55 +32,83 @@ export default function TakePicture(props) {
   }, [webcamRef]);
   return (
     <>
-      <StyledTakePicture hidden={webcamHidden}>
-        <Webcam
-          audio={false}
-          ref={webcamRef}
-          screenshotFormat="image/jpeg"
-          // videoConstraints={videoConstraints}
-        />
+      {!webcamHidden ? (
         <div>
-          <Button
-            onClick={capture}
-            variant="contained"
-            className={classes.button}
-          >
-            Take picture
-          </Button>
+          <Webcam
+            audio={false}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            // videoConstraints={videoConstraints}
+          />
+          <div>
+            <Button
+              onClick={capture}
+              variant="contained"
+              className={classes.button}
+              color="primary"
+            >
+              Take picture
+            </Button>
+          </div>
         </div>
-      </StyledTakePicture>
-
-      <StyledTakePicture hidden={!webcamHidden}>
-        <ProvisionalPicture data={provisionalPicture} />
-        <div>
-          <Button
-            onClick={e => saveEmotionData(provisionalEmotionData)}
-            variant="contained"
-            className={classes.button}
-          >
-            Add Picture to graph
-          </Button>
-          <Button
-            onClick={e => saveEmotionData(provisionalEmotionData)}
-            variant="contained"
-            className={classes.button}
-          >
-            Take another picture
-          </Button>
+      ) : (
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <div>
+            <div>
+              <img alt="provisionalPicture" src={provisionalPicture} />
+            </div>
+            <div>
+              <Button
+                onClick={e => saveEmotionData(provisionalEmotionData)}
+                variant="contained"
+                className={classes.button}
+                color="primary"
+                style={{ margin: "5px" }}
+              >
+                Add Picture To Graph
+              </Button>
+              <Button
+                onClick={e => setWebcamHidden(false)}
+                variant="contained"
+                className={classes.button}
+                style={{ margin: "5px" }}
+                color="secondary"
+              >
+                Discard Picture
+              </Button>
+            </div>
+          </div>
+          <div style={{ marginLeft: "20px", textAlign: "left" }}>
+            <p>
+              anger: {`${Math.round(provisionalEmotionData.emotion.anger)}%`}
+            </p>
+            <p>
+              disgust:{" "}
+              {`${Math.round(provisionalEmotionData.emotion.disgust)}%`}
+            </p>
+            <p>fear: {`${Math.round(provisionalEmotionData.emotion.fear)}%`}</p>
+            <p>
+              happiness:{" "}
+              {`${Math.round(provisionalEmotionData.emotion.happiness)}%`}
+            </p>
+            <p>
+              neutral:{" "}
+              {`${Math.round(provisionalEmotionData.emotion.neutral)}%`}
+            </p>
+            <p>
+              sadness:{" "}
+              {`${Math.round(provisionalEmotionData.emotion.sadness)}%`}
+            </p>
+            <p>
+              surprise:{" "}
+              {`${Math.round(provisionalEmotionData.emotion.surprise)}%`}
+            </p>
+          </div>
         </div>
-      </StyledTakePicture>
+      )}
     </>
   );
 }
-// {
-/* <h1>anger: {`${provisionalEmotionData.emotion.anger}`}</h1>
-<h1>disgust: {`${provisionalEmotionData.emotion.disgust}`}</h1>
-<h1>fear: {`${provisionalEmotionData.emotion.fear}`}</h1>
-<h1>happiness: {`${provisionalEmotionData.emotion.happiness}`}</h1>
-<h1>neutral: {`${provisionalEmotionData.emotion.neutral}`}</h1>
-<h1>sadness: {`${provisionalEmotionData.emotion.sadness}`}</h1>
-<h1>surprise: {`${provisionalEmotionData.emotion.surprise}`}</h1> */
-// }
 
 TakePicture.propTypes = {
   saveEmotionData: PropTypes.func.isRequired
--- src/styled.js
+++ src/styled.js
@@ -2,16 +2,6 @@ import styled from "styled-components";
 import AppBar from "@material-ui/core/AppBar";
 import Box from "@material-ui/core/Box";
 
-const StyledTakePicture = styled.div`
-  // display: flex;
-  // flex-direction: column;
-  // display: grid;
-  // grid-template-columns: 1fr;
-  // grid-template-rows: 40% 40% 20%;
-  // grid-template-rows: 400px 400px 100px;
-  // grid-template-columns: 20px auto 20px;
-`;
-
 const StyledAppBar = styled(AppBar)`
   align-items: center;
 `;
@@ -19,4 +9,5 @@ const StyledAppBar = styled(AppBar)`
 const StyledBox = styled(Box)`
   text-align: center;
 `;
-export { StyledTakePicture, StyledAppBar, StyledBox };
+
+export { StyledAppBar, StyledBox };
