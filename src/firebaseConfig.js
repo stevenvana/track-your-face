4caf2b80de4a989b017a6ab19ee5446f51a972fa
stevenspyramid <steven.vanasbeck@gmail.com>
2019-11-08 17:42:41 +0100

    put apikey of firebaseConfig in .env file

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCrIkW-ljxptHzUFhcBz_3YiDdIE6VzxW4",
+  apiKey: process.env.REACT_APP_APIKEY,
   authDomain: "track-your-face-982cc.firebaseapp.com",
   databaseURL: "https://track-your-face-982cc.firebaseio.com",
   projectId: "track-your-face-982cc",
   storageBucket: "track-your-face-982cc.appspot.com",
   messagingSenderId: "467544441315",
-  appId: "1:467544441315:web:9bb9ec503c6ca88c18a0d6"
+  appId: "1:467544441315:web:702186351a50b50118a0d6"
 };
-
 export default firebaseConfig;
